40a038eaf4dae9dba8bd6de4f719937771a36290
Sagnik <sagnikskb0nyp437@gmail.com>
2025-10-03 10:10:50 +0530

    feat: add endpoint to decrement available seats
    - Introduced PUT /schedules/{id}/decrement endpoint in ScheduleController
    - Added logic to safely reduce availableSeats for a given schedule

--- schedule/src/main/resources/data.sql
+++ schedule/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO Schedule (schedule_id, train_id, schedule_date, total_seats, available_seats, created_at)
+INSERT INTO Schedule (train_id, schedule_date, total_seats, available_seats, created_at)
 VALUES
-(5001, 101, '2025-09-15', 800, 750, NOW()),
-(5002, 101, '2025-09-16', 800, 800, NOW()),
-(5003, 102, '2025-09-15', 600, 550, NOW()),
-(5004, 102, '2025-09-16', 600, 590, NOW()),
-(5005, 103, '2025-09-15', 400, 300, NOW());
+(101, '2025-09-15', 800, 750, NOW()),
+(101, '2025-09-16', 800, 800, NOW()),
+(102, '2025-09-15', 600, 550, NOW()),
+(102, '2025-09-16', 600, 590, NOW()),
+(103, '2025-09-15', 400, 300, NOW());
